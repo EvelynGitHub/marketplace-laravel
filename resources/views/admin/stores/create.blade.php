298686a8f9b08d489d3d7c7dc00e7d108abc492f
Evelyn <evelynbrandao15@gmail.com>
2020-11-25 18:39:09 -0300

    Adicionando estrutura inicial com CRUDE de loja comppleto

---
+++ resources/views/admin/stores/create.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h1>Criar Loja</h1>
+
+<form action="{{route('admin.stores.store')}}" method="post">
+    <input type="hidden" name="_token" value="{{csrf_token()}}">
+    <div class="form-group" class="form-group">
+        <label for="name">Nome da Loja</label>
+        <input type="text" name="name" class="form-control">
+    </div>
+
+    <div class="form-group">
+        <label for="description">Descrição</label>
+        <input type="text" name="description" class="form-control">
+    </div>
+
+    <div class="form-group">
+        <label for="phone">Telefone</label>
+        <input type="text" name="phone" class="form-control">
+    </div>
+
+    <div class="form-group">
+        <label for="mobile_phone">Celular/Whatsapp</label>
+        <input type="text" name="mobile_phone" class="form-control">
+    </div>
+
+    <div class="form-group">
+        <label for="slug">Slug</label>
+        <input type="text" name="slug" class="form-control">
+    </div>
+
+    <div class="form-group">
+        <label for="user">Usuário</label>
+        <select name="user" class="form-control">
+            @foreach($users as $user)
+            <option value="{{$user->id}}">{{$user->name}}</option>
+            @endforeach
+        </select>
+    </div>
+
+    <div class="form-group">
+        <button type=" submit" class="btn btn-lg btn-success">Criar loja</button>
+    </div>
+</form>
+@endsection
---
+++ resources/views/admin/stores/edit.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h1>Criar Loja</h1>
+
+<form action="{{route('admin.stores.update', ['store'=> $store->id])}}" method="post">
+    <input type="hidden" name="_token" value="{{csrf_token()}}">
+    <div class="form-group" class="form-group">
+        <label for="name">Nome da Loja</label>
+        <input type="text" name="name" class="form-control" value="{{$store->name}}">
+    </div>
+
+    <div class="form-group">
+        <label for="description">Descrição</label>
+        <input type="text" name="description" class="form-control" value="{{$store->description}}">
+    </div>
+
+    <div class="form-group">
+        <label for="phone">Telefone</label>
+        <input type="text" name="phone" class="form-control" value="{{$store->phone}}">
+    </div>
+
+    <div class="form-group">
+        <label for="mobile_phone">Celular/Whatsapp</label>
+        <input type="text" name="mobile_phone" class="form-control" value="{{$store->mobile_phone}}">
+    </div>
+
+    <div class="form-group">
+        <label for="slug">Slug</label>
+        <input type="text" name="slug" class="form-control" value="{{$store->slug}}">
+    </div>
+
+    <div class="form-group">
+        <button type=" submit" class="btn btn-lg btn-success">Atualizar loja</button>
+    </div>
+</form>
+@endsection
