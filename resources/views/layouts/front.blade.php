afc834fe44e1653c55968ef5efefd89ba819f2ad
Evelyn <evelynbrandao15@gmail.com>
2020-12-03 19:47:50 -0300

    Ajuste em front e autenticação de ROLE_USER e ROLE_OWNER

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -40,36 +40,20 @@
                 @endforeach
             </ul>
 
-            @auth
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item @if(request()->is('admin/stores*')) active @endif">
-                    <a class="nav-link" href="{{route('admin.stores.index')}}">Lojas <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item @if(request()->is('admin/products*')) active @endif">
-                    <a class="nav-link" href="{{route('admin.products.index')}}">Produtos</a>
-                </li>
-                <li class="nav-item @if(request()->is('admin/categories*')) active @endif">
-                    <a class="nav-link" href="{{route('admin.categories.index')}}">Categorias</a>
-                </li>
-            </ul>
-
             <div class="my-2 my-lg-0">
                 <ul class="navbar-nav mr-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#" onclick="event.preventDefault();
-                                                                  document.querySelector('form.logout').submit(); ">Sair</a>
-
-                        <form action="{{route('logout')}}" class="logout" method="POST" style="display:none;">
-                            @csrf
-                        </form>
-                    </li>
-                    <li class="nav-item">
-                        <span class="nav-link">{{auth()->user()->name}}</span>
-                    </li>
 
                     @auth
                     <li class="nav-item @if(request()->is('my-orders')) active @endif">
                         <a href="{{route('user.orders')}}" class="nav-link">Meus Pedidos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" onclick="event.preventDefault(); document.querySelector('form.logout').submit();">Sair</a>
+
+                        <form action="{{route('logout')}}" class="logout" method="POST" style="display:none;">
+                            @csrf
+                        </form>
+
                     </li>
                     @endauth
                     <li class="nav-item">
@@ -83,7 +67,6 @@
 
                 </ul>
             </div>
-            @endauth
 
         </div>
     </nav>
