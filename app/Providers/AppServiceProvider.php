b12f8c67978a4685759228e2632cbc75aacd3f8f
Evelyn <evelynbrandao15@gmail.com>
2020-12-04 12:50:07 -0300

    version legacy de factory, e definindo uso do bootstrap

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+// use Illuminate\Contracts\Pagination\Paginator;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Paginator::useBootstrap();
         \PagSeguro\Library::initialize();
         \PagSeguro\Library::cmsVersion()->setName("Markteplace")->setRelease("1.0.0");
         \PagSeguro\Library::moduleVersion()->setName("Markteplace")->setRelease("1.0.0");
